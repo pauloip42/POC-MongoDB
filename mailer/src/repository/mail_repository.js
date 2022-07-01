c902b52cd42b44ec7563eeee6da0c20e8d78856b
Paulo Lopes <phlj.junior@gmail.com>
2022-07-01 16:27:03 -0300

    :sparkles: repository - find and update

--- mailer/src/repository/mail_repository.js
+++ mailer/src/repository/mail_repository.js
@@ -1,9 +1,26 @@
 const { MailModel } = require('../model/mail_model');
+const mongoose = require('mongoose');
 
 const MailRepository = {
     async insertEmail(data) {
         const model = new MailModel(data);
         const result = await model.save();
+        return result;
+    },
+
+    async findOneEmail(email) {
+        const result = await MailModel.findOne({email});
+        return result;
+    },
+
+    async updateOneEmail(id) {
+        const obId = mongoose.Types.ObjectId(id);
+        const result = await MailModel.findOneAndUpdate(
+            {_id: obId}, 
+            {email: 'novoemail@email.com'}, 
+            {new: true}
+        );
+        
         return result;
     }
 }
